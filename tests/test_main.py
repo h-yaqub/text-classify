5e16a14c40b5399e2e59eae9249dc1db76fd9f18
Hammad Yaqub <h_y-r@outlook.com>
2024-06-22 18:32:07 +0500

    Fix tests lint

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,15 @@
 import unittest
+
 from core.utils import Utils
+
 
 class TestDataProcessing(unittest.TestCase):
     def test_read_data(self):
-        data = Utils().read_data('data/classes/dev.txt')
+        data = Utils().read_data("data/classes/dev.txt")
         self.assertIsInstance(data, list)
 
     def test_create_dict(self):
-        data = Utils().read_data('data/classes/dev.txt')
+        data = Utils().read_data("data/classes/dev.txt")
         word_to_index = {"<unk>": 0}
         tag_to_index = {}
         Utils().create_dict(data, word_to_index, tag_to_index)
@@ -15,12 +17,13 @@ class TestDataProcessing(unittest.TestCase):
         self.assertGreater(len(tag_to_index), 0)
 
     def test_create_tensor(self):
-        data = Utils().read_data('data/classes/dev.txt')
+        data = Utils().read_data("data/classes/dev.txt")
         word_to_index = {"<unk>": 0}
         tag_to_index = {}
         Utils().create_dict(data, word_to_index, tag_to_index)
         tensors = list(Utils().create_tensor(data, word_to_index, tag_to_index))
         self.assertIsInstance(tensors[0], tuple)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
